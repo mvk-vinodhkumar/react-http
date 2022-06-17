ab4ec1f6d2b20e968d71cafa97b1137ac8db69f5
mvk-vinodhkumar <mvk.vinodhkumar@gmail.com>
2022-06-17 13:45:26 +0530

    Updates

---
+++ precache-manifest.f1a025fd63dcea48b04edc2867595a86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "230e6a59783d79d42a98f7859c2f11e2",
+    "url": "/react-http/index.html"
+  },
+  {
+    "revision": "e06fb295ab5f531d9868",
+    "url": "/react-http/static/css/main.08f933cb.chunk.css"
+  },
+  {
+    "revision": "9be2568482fc1b1ae314",
+    "url": "/react-http/static/js/2.6426c65b.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/react-http/static/js/2.6426c65b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e06fb295ab5f531d9868",
+    "url": "/react-http/static/js/main.822bc1dc.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/react-http/static/js/main.822bc1dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c58965eb74aa3199329",
+    "url": "/react-http/static/js/runtime-main.2a60e502.js"
+  }
+]);
